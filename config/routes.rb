5e3a759fe258ca98055c1fe13f15e498bf9b5a1c
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-06-22 11:29:00 -0700

    create route for events display

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do # /api/data
-
     get '/data', to: 'tests#index'
-    
-    resources :dogs
-
+    get '/events', to: 'events#index' 
   end
 
   get '*path', to: "static_pages#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
-
 end
