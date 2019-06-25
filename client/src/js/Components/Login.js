d1cb57dd64867d214bc749cbb034df5b882ca968
shadeying <shadeying@gmail.com>
2019-06-25 21:22:02 +0000

    Login implemented

---
+++ client/src/js/Components/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import SpotifyLogin from 'react-spotify-login';
+
+const onSuccess = response => console.log(response);
+const onFailure = response => console.error(response);
+
+function Login(props) {
+  return (
+    <div className="login-page">
+      <div className="login-page-background"></div>
+      <section className="login-form">
+        <h1>Welcome to Jetify</h1>
+        <SpotifyLogin type="button" className="btn btn-dark"
+          clientId={"7a7698ee205a4b01ac2a987c6054da8b"}
+          redirectUri={"http://localhost:3000/api/logging-in"}
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+        />
+      </section>
+    </div>
+  );
+}
+
+export default Login;
