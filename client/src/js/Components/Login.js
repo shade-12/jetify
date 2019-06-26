c08592d2ab754eca6fe9a731bba19274ec4f2ea2
shadeying <shadeying@gmail.com>
2019-06-26 01:16:20 +0000

    Login button centered

--- client/src/js/Components/Login.js
+++ client/src/js/Components/Login.js
@@ -4,6 +4,7 @@ import SpotifyLogin from 'react-spotify-login';
 
 const onSuccess = response => console.log(response);
 const onFailure = response => console.error(response);
+const buttonText = <div><img src="https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg" alt="login-logo"/>&nbsp;&nbsp;<span>Login with Spotify</span></div>;
 
 function Login(props) {
   return (
@@ -11,9 +12,13 @@ function Login(props) {
       <div className="login-page-background"></div>
       <section className="login-form">
         <h1>Welcome to Jetify</h1>
-        <SpotifyLogin type="button" className="btn btn-dark"
+        <SpotifyLogin
+          type="button"
+          className="btn btn-dark"
+          buttonText={buttonText}
           clientId={"7a7698ee205a4b01ac2a987c6054da8b"}
           redirectUri={"http://localhost:3000/api/logging-in"}
+          scope={"user-read-private user-read-currently-playing user-library-modify playlist-modify-public playlist-read-collaborative playlist-read-private playlist-modify-private"}
           onSuccess={onSuccess}
           onFailure={onFailure}
         />
