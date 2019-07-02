9027f7155b87395c52f1f8eee286e2270ef8effb
shadeying <shadeying@gmail.com>
2019-07-02 05:36:10 +0000

    Try deploy

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     end
   end
 
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  get '*path', to: "static_pages#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
 end
