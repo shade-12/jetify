9d4e0940da23afc2b723a7382ace9b63a0a471bd
shadeying <shadeying@gmail.com>
2019-07-02 00:36:39 +0000

    Use hashrouter

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -14,18 +14,18 @@ class App extends Component {
 
   render() {
     return (
-      <Router basename="/">
+      <Router basename="/jetify.herokuapp.com">
         <div className="App">
           <Route
-            path="/" exact
+            path={`${process.env.PUBLIC_URL}/`} exact
             render={() => <LoginPage cookies={this.props.cookies} handleLogin={this.handleLogin} />}
           />
           <Route
-            path="/users/:id" exact
+            path={`${process.env.PUBLIC_URL}/users/:id`} exact
             render={() => <UserPage cookies={this.props.cookies} />}
           />
           <Route
-            path="/users/:id/history" exact
+            path={`${process.env.PUBLIC_URL}/users/:id/history`} exact
             render={() => <HistoryPage cookies={this.props.cookies}/>}
           />
         </div>
