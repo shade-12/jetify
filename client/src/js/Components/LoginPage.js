6492df55178d3abaf3fbb7e1458c715dc6dfb2d9
shadeying <shadeying@gmail.com>
2019-07-02 23:46:51 +0000

    CHange redirect uri

--- client/src/js/Components/LoginPage.js
+++ client/src/js/Components/LoginPage.js
@@ -72,7 +72,7 @@ class LoginPage extends Component {
             className="btn btn-dark"
             buttonText={buttonText}
             clientId={process.env.REACT_APP_SPOTIFY_CLIENT_ID}
-            redirectUri={"http://localhost:3000/api/logging-in"}
+            redirectUri={"https://jetify.herokuapp.com/api/logging-in"}
             scope={"user-read-email user-read-private user-read-currently-playing user-library-modify playlist-modify-public playlist-read-collaborative playlist-read-private playlist-modify-private"}
             onSuccess={this.onSuccess}
             onFailure={this.onFailure}
